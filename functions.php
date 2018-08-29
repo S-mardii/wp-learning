a690000948066ba3b8df6a03eccb3eb9f670e5dd
Mardy <mardy@odc.org>
2018-08-29 17:07:41 +0700

    [TASK] WP-4: Develop Child Navigation

--- functions.php
+++ functions.php
@@ -7,3 +7,14 @@ function custom_theme_assets() {
 add_action( 'wp_enqueue_scripts', 'custom_theme_assets' );
 
 register_nav_menus( ['primary' => __( 'Primary Menu' )] );
+
+function get_the_top_ancestor_id() {
+	global $post;
+
+	if ( $post->post_parent ) {
+		$ancestors = array_reverse( get_post_ancestors( $post->ID ) );
+		return $ancestors[0];
+	} else {
+		return $post->ID;
+	}
+}
