42a5c64c68b7de4aa8c56593e867f725ac3f0dfe
Mardy <mardy@odc.org>
2018-08-30 18:32:08 +0700

    [TASK] WP-9: Search

--- search.php
+++ search.php
@@ -3,6 +3,9 @@
 get_header();
 
 if ( have_posts() ) {
+?>
+	<h2>Search result for query: <?php the_search_query(); ?></h2>
+	<?php
 	while ( have_posts() ) {
 		the_post();
 		get_template_part('thepost');
