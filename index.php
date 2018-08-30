8cd80ceb8a9a25e2ea1e15eb24b322906825d287
Mardy <mardy@odc.org>
2018-08-30 17:38:49 +0700

    [TASK] WP-8: Feature Image

--- index.php
+++ index.php
@@ -7,6 +7,12 @@ if ( have_posts() ) {
 		the_post();
 ?>
 		<article>
+			<?php if ( has_post_thumbnail() ) { ?>
+				<div class="small-thumbnail">
+					<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'small-thumbnail' ); ?></a>
+				</div>
+			<?php } ?>
+			
 			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 			<p class="post-meta"><?php the_time( 'F jS, Y' ); ?> | 
 				<a href="<?php echo get_author_posts_url( get_the_author_meta('ID'), get_the_author_meta('user_nicename') ); ?>">
