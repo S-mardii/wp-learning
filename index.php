5f3f9798b53a81bcb6a13ca2671582fd765e4330
Mardy <mardy@odc.org>
2018-09-03 16:44:27 +0700

    [TASK] WP-11: Widget

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
 <?php
 
-get_header();
+get_header(); ?>
 
-if ( have_posts() ) {
-	while ( have_posts() ) {
-		the_post();
-		get_template_part( 'thepost', get_post_format() );
-	}
-} else {
-	echo "<p>There are no posts!</p>";
-}
+<div class="main-content clearfix">
+	<div class="main-column">
+		<?php
+		if ( have_posts() ) {
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'thepost', get_post_format() );
+			}
+		} else {
+			echo "<p>There are no posts!</p>";
+		}
+		?>
+	</div> <!-- main-column -->
 
-get_footer();
+	<?php if ( is_active_sidebar( 'rightsidebar' ) ) { ?>
+		<div class="sidebar-column">
+			<?php dynamic_sidebar( 'rightsidebar' ); ?>
+		</div> <!-- sidebar-column -->
+	<?php } ?>
+	
+</div> <!-- main-content -->
+
+<?php get_footer(); ?>
